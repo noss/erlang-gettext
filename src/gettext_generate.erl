228b8a8ef0ae49b460a085ee324add1d66ebe5e2
Christian Sunesson <chsu79@gmail.com>
2009-04-14 21:56:00 +0200

    Building AST and compiling directly.
    + plus adding spanish.po for educational value.

--- src/gettext_generate.erl
+++ src/gettext_generate.erl
@@ -7,29 +7,57 @@
 
 -compile(export_all).
 
+compile(Modname, PO) ->
+    compile(Modname, PO, []).
 
-write([Mod, PO]) ->
-    write(Mod, PO).
+compile(Modname, POFilename, Opts) ->
+    [_Header | Translations] = gettext:parse_po(POFilename),
+    
+    Forms = generate_forms(Modname, Translations),
+    
+    {ok, Modname, Code} = compile:forms(Forms, Opts),
+    
+    code:purge(Modname),
+    code:load_binary(Modname, POFilename, Code).
 
-write(Mod, PO) ->
-    Content = generate(Mod, PO),
-    file:write_file(lists:concat([Mod, '.erl']), Content).
+generate_forms(Modname, Translations) ->
+    ModuleAst  = erl_syntax:attribute(
+		   erl_syntax:atom(module), 
+		   [erl_syntax:atom(Modname)]),
+    
+    ExportAst = erl_syntax:attribute(
+		  erl_syntax:atom(export),
+		  [erl_syntax:list(
+		     [erl_syntax:arity_qualifier(
+			erl_syntax:atom(gettext), 
+			erl_syntax:integer(1))
+		     ])
+		  ]),
+    
+    
+    FunctionAst = generate_function(Translations),
+    
+    [erl_syntax:revert(Ast) || Ast <- [ModuleAst, ExportAst, FunctionAst]].
+    
 
-generate(Modname, POFilename) ->
-    Translations = gettext:parse_po(POFilename),
-    Head = ["-module(", atom_to_list(Modname), ").\n",
-	    "-export([gettext/1]).\n"],
-    Clauses = [generate_clause(K,V) || {K,V} <- Translations, not is_atom(K)],
-    EndClause = ["gettext(X) -> atom_to_list(X)"],
-    erlang:iolist_to_binary([Head, clause_join([Clauses|EndClause])]).
+generate_function(KVs) ->
+    Clauses = [generate_clause(K, V) || {K,V} <- KVs],
+    
+    X = erl_syntax:variable("X"),
+    ListToAtom = erl_syntax:atom(atom_to_list),
+    EndClause = erl_syntax:clause(
+		  [X], none,
+		  [erl_syntax:application(none, ListToAtom, [X])]),
+    
+    erl_syntax:function(erl_syntax:atom(gettext), Clauses ++ [EndClause]).
+
 
 generate_clause(K, V) ->
-    ["gettext('", K, "') ->\n",
-     "  \"", V,"\""].
+    erl_syntax:clause(
+      [erl_syntax:atom(K)], 
+      none, 
+      [erl_syntax:string(V)]
+     ).
 
-clause_join([Last]) ->
-    [Last, ".\n"];
-clause_join([H|Tl]) ->
-    [H, ";\n" | clause_join(Tl)].
 
 	      
